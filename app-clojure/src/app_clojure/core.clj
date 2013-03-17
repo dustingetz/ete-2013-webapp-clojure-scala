0afec08ad9c29aab09ab50abf634e246300bac4e
Dustin Getz <dgetz@wingspan.com>
2013-03-17 15:06:25 -0400

    lein new app-clojure

---
+++ app-clojure/src/app_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns app-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ app-clojure/test/app_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns app-clojure.core-test
+  (:use clojure.test
+        app-clojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
